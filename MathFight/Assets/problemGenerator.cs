58b798866ed03366f3facb72fad2c29a62dda0e6
Alex561 <alexchapp@yahoo.com>
2015-05-21 00:16:50 -0700

    lots of stuff click me to see
    2 text boxes
    countdown
    turn manager
    turn flipper
    lots of new GUI elements

--- MathFight/Assets/problemGenerator.cs
+++ MathFight/Assets/problemGenerator.cs
@@ -4,19 +4,24 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 public class problemGenerator : MonoBehaviour {
 	public Text problemText;
+	public Text turnText;
+	public Text timeText;
 	public List<string> unitCircle;
 	bool correct = false;
 	public int ap1 = 3;
 	public int ap2= 3;
 	bool p1Turn = true;
 	string question;
-	string answer = "eneter answer";
+	string answer1 = "p1";
+	string answer2 = "p2";
+	float countdown = 10.0f;
 
 	void Start () {
 
 		problemText.text = "placeholder";
 		createUnitCircle ();
 		question = changeProblem ();
+		turnText.text = "player 1's turn";
 	
 	}
 	void createUnitCircle()	{
@@ -30,7 +35,6 @@ public class problemGenerator : MonoBehaviour {
 		int n = Random.Range (0, unitCircle.Count);
 		problemText.text = unitCircle[n];
 		string q = (n.ToString() + "U");
-		print ("in change problem" + q);
 		return q;
 	}
 	void checkSolution(string a){
@@ -54,18 +58,44 @@ public class problemGenerator : MonoBehaviour {
 	}
 	
 	void OnGUI(){
-		answer = GUI.TextField(new Rect(290,350,200,20),answer,25);
+		answer1 = GUI.TextField(new Rect(190,600,200,20),answer1,25);
+		answer2 = GUI.TextField(new Rect(1000,600,200,20),answer2,25);
 	}
+
+	void flipTurn(){
+		p1Turn = ! p1Turn;
+		if (p1Turn == true) {
+			turnText.text = "player 1's turn";
+		}
+		if (p1Turn == false) {
+			turnText.text = "player 2's turn";
+		}
+		countdown = 6.1f;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Q)) {
-			//get input
-			print (question);// test
-			print(answer);
-			checkSolution("input");
-			print ("new problem");
-			question = changeProblem ();
+		//countdown
+		countdown -= Time.deltaTime;
+		countdown = Mathf.Round(countdown *100f)/100f;
+		timeText.text = countdown.ToString();
+		if (countdown <= 0) {
+			flipTurn();
 		}
+		//player 1 enters answer
+		if (Input.GetKeyDown (KeyCode.Q) && p1Turn) {
+		
+			checkSolution (answer1);//check if right
+			question = changeProblem ();//make a new question
+			flipTurn();
+		}
+		//player 2 eneters answer
+		if (Input.GetKeyDown (KeyCode.P)&& !p1Turn) {
+			checkSolution(answer2);
+			question = changeProblem ();
+			flipTurn ();
+		}
+		//add points
 		if (correct) {
 			correct=false;
 		}
