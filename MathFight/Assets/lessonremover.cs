0ccd0666f4293b10415d5cd777cc5dab58962cef
warstark <smacha@uci.edu>
2015-05-26 13:09:19 -0700

    @#$%#$%#$#@$%

--- MathFight/Assets/lessonremover.cs
+++ MathFight/Assets/lessonremover.cs
@@ -5,28 +5,31 @@ public class lessonremover : MonoBehaviour {
 	//public string[] dialogue={"Math Lessons","This is a test"};
 	//int index=0;
 	//Rect dialoguerect=new Rect(435,100,500,50);
-
+	public bool flip=false;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	/*void Update () {
+	void Update () {
 		if (Input.GetKeyDown ("return")) {
-			gameObject.SetActive(false);
+			flip=true;
 
 		}
 	
-	}*/
+	}
 
 	// this is suppose to make the text mesh fall from the screen when enter is pressed but it wont do that, please help.......
 	void OnTriggerEnter(Collider other)
-	{
-		if((Input.GetKeyDown("return"))&&(other.tag == "text_on_bag"))
-		{
-			other.attachedRigidbody.useGravity=true;
-			other.attachedRigidbody.isKinematic=false;
+	{ 
+		if ((flip == true)) {
+			if(other.tag == "text_on_bag")
+			{
+				Debug.Log ("what the fucks going on");
+				other.attachedRigidbody.useGravity = true;
+				other.attachedRigidbody.isKinematic = false;
+			}
 		}
 	}
 	/*void OnGUI()
