0a0fe38865e138b18d6092dac7d6ff219deeead5
Alex561 <alexchapp@yahoo.com>
2015-05-26 18:51:18 -0700

    fixed fight scene since sai broke it and added ap gui

--- MathFight/Assets/problemGenerator.cs
+++ MathFight/Assets/problemGenerator.cs
@@ -6,6 +6,8 @@ public class problemGenerator : MonoBehaviour {
 	public Text problemText;
 	public Text turnText;
 	public Text timeText;
+	public Text Ap1;
+	public Text Ap2;
 	public List<string> unitCircle;
 	bool correct = false;
 	public int ap1 = 3;
@@ -14,7 +16,7 @@ public class problemGenerator : MonoBehaviour {
 	string question;
 	string answer1 = "p1";
 	string answer2 = "p2";
-	float countdown = 10.0f;
+	float countdown = 12.0f;
 
 	void Start () {
 
@@ -26,8 +28,8 @@ public class problemGenerator : MonoBehaviour {
 	}
 	void createUnitCircle()	{
 		unitCircle.Add ("What is sin30°?");
-		unitCircle.Add ("What is tan30°?");
-		unitCircle.Add ("What is sin45°?");
+		unitCircle.Add ("What is cos30°?");
+		unitCircle.Add ("What is tan45°?");
 		unitCircle.Add ("What is cos60°?");
 		unitCircle.Add ("What is cos90°?");
 	}
@@ -45,13 +47,25 @@ public class problemGenerator : MonoBehaviour {
 			else
 				return ap-=1;
 		case "1U":
-			return ap;
+			if (a == "s3/2")
+				return ap=+1;
+			else 
+				return ap -=1;
 		case "2U":
-			return ap;
+			if (a == "1")
+				return ap=+1;
+			else 
+				return ap -=1;
 		case "3U":
-			return ap;
+			if (a == "1/2")
+				return ap=+1;
+			else 
+				return ap -=1;
 		case "4U":
-			return ap;
+			if (a == "0")
+				return ap=+1;
+			else 
+				return ap -=1;
 		default:
 			return ap;
 		}
@@ -70,7 +84,7 @@ public class problemGenerator : MonoBehaviour {
 		if (p1Turn == false) {
 			turnText.text = "player 2's turn";
 		}
-		countdown = 10f;
+		countdown = 12f;
 	}
 
 	// Update is called once per frame
@@ -86,14 +100,18 @@ public class problemGenerator : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Q) && p1Turn) {
 		
 			ap1 = checkSolution(answer1,ap1);//check if right
-			print ("ap1: " + ap1.ToString());
+			if (ap1<0)
+				ap1 = 0;
+			Ap1.text = ("ap1: " + ap1.ToString());
 			question = changeProblem ();//make a new question
 			flipTurn();
 		}
 		//player 2 eneters answer
 		if (Input.GetKeyDown (KeyCode.P)&& !p1Turn) {
 			ap2 = checkSolution(answer2,ap2);
-			print("ap2: " + ap2.ToString());
+			if (ap2<0)
+				ap2 = 0;
+			Ap2.text = ("ap2: " + ap2.ToString());
 			question = changeProblem ();
 			flipTurn ();
 		}
