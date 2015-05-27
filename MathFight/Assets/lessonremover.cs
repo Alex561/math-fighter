c06af850b7b8ba092ee2a775f75a6de3649944c4
warstark <smacha@uci.edu>
2015-05-26 21:54:56 -0700

    training scene complete

--- MathFight/Assets/lessonremover.cs
+++ MathFight/Assets/lessonremover.cs
@@ -7,7 +7,8 @@ public class lessonremover : MonoBehaviour {
 	//Rect dialoguerect=new Rect(435,100,500,50);
 	public bool flip=false;
 	public string name;
-	public int count=0;
+	public int input_count=0;
+	public int output_count=0;
 	Vector3 bleh=new Vector3(5.47f,-1.52f,-27.06f);
 	// Use this for initialization
 	void Start () {
@@ -22,37 +23,55 @@ public class lessonremover : MonoBehaviour {
 
 		if (Input.GetKeyDown("space"))
 			{
-
+			 
 			Debug.Log ("return");
-			flip=true;
 			Debug.Log ("what the fucks going on");
-			GetComponent<TextMesh>().text = "what is the angle for tan 45?";
-			Debug.Log("the fucks wrong with you");
-			count++;
-			if(count==1)
+			if(input_count==0)
 			{
-				if(Input.GetKeyDown("space"))
-				   {
+			GetComponent<TextMesh>().text = "what is the angle for tan 45?";
+			}
+			Debug.Log("the fucks wrong with you");
+
+			if(input_count==1)
+			{
+				   
 					GetComponent<TextMesh>().text = "what is the angle for cos 60?";
-					}
 			}
 			//GetComponent<Collider>().attachedRigidbody.useGravity = true;
 			//GetComponent<Collider>().attachedRigidbody.isKinematic = false;
-
-
+			if(input_count==2)
+			{
+				GetComponent<TextMesh>().text = "what is the angle for sin 60?";
+			}
+			input_count++;
 		}
 
 		if (Input.GetKeyDown (KeyCode.G)) {
-			if (name == "1") { 
-				flip = false;
+
+			if (name == "1"&& output_count ==0) { 
+
+				Debug.Log("ok");
 				GetComponent<Collider> ().attachedRigidbody.position = bleh;
 				GetComponent<Collider> ().attachedRigidbody.isKinematic = true;
 				GetComponent<TextMesh> ().text = "correct";
+
+			}
+			else if((name=="1/2")&&(output_count==1))
+			{
+				GetComponent<TextMesh> ().text = "correct";
+			}
+			else if(name=="s3/2"&&output_count==2)
+			{
+				GetComponent<TextMesh> ().text = "correct";
 			}
 			else {
-				GetComponent<TextMesh> ().text = "wrong bitch!";
-	
+
+				GetComponent<TextMesh> ().text ="Unit circle \n Angle	Sin	   Cos	 Tan=Sin/Cos \n 30°	1/2	  √3/2	 1√3 =  √33 \n 45°	√2/2	√2/2	1 \n 60°	√3/2	  1/2	√3";
+				input_count--;
+				output_count--;
+													
 			}
+			output_count++;
 		}
 }
 }
