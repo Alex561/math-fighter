0e1d7a5742b04269144af3a42a6c38c05f2af37f
Alex561 <alexchapp@yahoo.com>
2015-05-26 20:13:19 -0700

    prelim player controller

---
+++ MathFight/Assets/p1Controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class p1Controller : MonoBehaviour {
+	public problemGenerator pg;
+	// Use this for initialization
+	void Start () {
+		pg = gameObject.GetComponent<problemGenerator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.W))
+		    {
+
+			if (pg ==null)
+				Debug.Log("null");
+			///call punch
+		}
+
+	}
+}
