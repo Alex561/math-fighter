7135f58ecf16e8658be2779e12c72ee4e0f51f21
Alex561 <alexchapp@yahoo.com>
2015-05-30 14:20:22 -0700

    finished multiple choice

--- MathFight/Assets/p1Controller.cs
+++ MathFight/Assets/p1Controller.cs
@@ -19,11 +19,46 @@ public class p1Controller : MonoBehaviour {
 			
 			pg.ap1 -=1;
 			Debug.Log("w");
+			//put punch animation
+			//put punch sfx
 			hp2.health -=1;
 		
 		}
-		if (!Input.GetKeyDown(KeyCode.W)){
-
+		if (Input.GetKeyDown(KeyCode.Z)&& pg.p1Turn){
+			if(pg.question== ("0U") || pg.question == ("3U"))
+				pg.ap1+=1;
+			else
+				pg.ap1-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
+		}
+		if (Input.GetKeyDown(KeyCode.C)&& pg.p1Turn){
+			if(pg.question== ("4U") )
+				pg.ap1+=1;
+			else
+				pg.ap1-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
+		}
+		if (Input.GetKeyDown(KeyCode.X)&& pg.p1Turn){
+			if(pg.question== ("1U"))
+				pg.ap1+=1;
+			else
+				pg.ap1-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
+		}
+		if (Input.GetKeyDown(KeyCode.V)&& pg.p1Turn){
+			if(pg.question== ("2U") )
+				pg.ap1+=1;
+			else
+				pg.ap1-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
 		}
 		
 	}
--- MathFight/Assets/p2Controller.cs
+++ MathFight/Assets/p2Controller.cs
@@ -22,6 +22,42 @@ public class p2Controller : MonoBehaviour {
 			hp1.health-=1;
 			///call punch
 		}
-		
+		if (Input.GetKeyDown(KeyCode.Alpha1)&& !pg.p1Turn){
+			if(pg.question== ("0U") || pg.question == ("3U"))
+				pg.ap2+=1;
+			else
+				pg.ap2-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2)&& !pg.p1Turn){
+			if(pg.question== ("4U") )
+				pg.ap2+=1;
+			else
+				pg.ap2-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha3)&& pg.p1Turn){
+			if(pg.question== ("1U"))
+				pg.ap2+=1;
+			else
+				pg.ap2-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4)&& pg.p1Turn){
+			if(pg.question== ("2U") )
+				pg.ap2+=1;
+			else
+				pg.ap2-=1;
+			Debug.Log("Changing Problem");
+			pg.question = pg.changeProblem();
+			pg.flipTurn ();
+		}
+
 	}
 }
