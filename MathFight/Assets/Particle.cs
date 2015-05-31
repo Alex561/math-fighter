87231f70b8cf17d4216ebf343517884273b21c9d
TinaFeng <xuanfeng1996@gmail.com>
2015-05-30 17:13:20 -0700

    Particle effect script and particle system prefab

---
+++ MathFight/Assets/Particle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : MonoBehaviour {
+	public ParticleSystem Flame;
+	public bool enableEmission;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)){
+			Flame.enableEmission = true;
+			Debug.Log("lol");
+		}
+
+		if(Input.GetKeyUp(KeyCode.A)){
+			Flame.enableEmission = false;
+		};
+		;
+		
+
+	
+	}
+}
