6ef40b4dd0edeaef776a46f9d31d723dbad4c43f
TinaFeng <xuanfeng1996@gmail.com>
2015-05-31 16:16:44 -0700

    clean up

---
+++ MathFight/Assets/Scripts/Sangbag_collision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sangbag_collision : MonoBehaviour {	
+	Animator anim_sand;
+	// Use this for initialization
+	void Start () {
+		anim_sand = GetComponent<Animator> ();
+	}
+	
+	void Update ()
+	{
+		AnimatorStateInfo stateinfo = anim_sand.GetCurrentAnimatorStateInfo (0);
+
+		}
+	void OnTriggerEnter(Collider other) {
+		if (gameObject.tag == "Batman"){
+			anim_sand.SetTrigger("Shake");
+			Debug.Log ("bam");
+	}
+	}
+}
